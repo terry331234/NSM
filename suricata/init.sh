20de34ef8105198ba3dd30ae217a589083a115fb
terry331234 <terry331234@gmail.com>
2022-03-07 14:03:21 +0800

    Add suricata update script

---
+++ suricata/init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+suricata-update list-sources --enabled | grep snort/community > /dev/null
+if [[ $? != 0 ]]; then
+    echo "Adding snort community rules"
+    suricata-update add-source snort/community https://www.snort.org/downloads/community/community-rules.tar.gz
+fi
+
+suricata-update update-sources
+suricata-update --no-test
+exec /bin/bash /docker-entrypoint.sh $@
